0c8868eb54c81b1f3a40921a9a028eb4b765eec7
jaeem006 <javierem_94@ciencias.unam.mx>
2023-01-27 21:50:09 -0600

    sesion 9

--- sesion-09/sesion-09/src/main/java/com/example/sesion09/security/JWTAuthorizationFilter.java
+++ sesion-09/sesion-09/src/main/java/com/example/sesion09/security/JWTAuthorizationFilter.java
@@ -1,4 +1,32 @@
 package com.example.sesion09.security;
 
-public class JWTAuthorizationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JWTAuthorizationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String bearerToken = request.getHeader("Authorization");
+
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")){
+            String token = bearerToken.replace("Bearer ", "");
+
+            UsernamePasswordAuthenticationToken usernamePAT = TokenUtils.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(usernamePAT);
+        }
+
+        filterChain.doFilter(request, response);
+    }
 }
