f9b5e46af421ce0c80e07e824f8d3933ae9c5c55
jaeem006 <javierem_94@ciencias.unam.mx>
2023-01-27 21:56:19 -0600

    sesion 9

--- sesion-09/sesion-09/src/main/java/com/example/sesion09/security/WebSecurityConfig.java
+++ sesion-09/sesion-09/src/main/java/com/example/sesion09/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.sesion09.security;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,13 +14,23 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@AllArgsConstructor
 public class WebSecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+    private final JWTAuthorizationFilter jwtAuthorizationFilter;
 
     @Bean
     SecurityFilterChain filterChain (HttpSecurity http,
                         AuthenticationManager authManager) throws Exception {
+
+        JWTAuthenticationFilter  jwtAuthenticationFilter = new JWTAuthenticationFilter();
+        jwtAuthenticationFilter.setAuthenticationManager(authManager);
+        jwtAuthenticationFilter.setFilterProcessesUrl("/login");
+
         return http
                 .csrf().disable()
                 .authorizeRequests()
@@ -31,10 +42,12 @@ public class WebSecurityConfig {
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
+                .addFilter(jwtAuthenticationFilter)
+                .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
-    @Bean
+    /*@Bean
     UserDetailsService userDetailsService (){
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
 
@@ -43,12 +56,12 @@ public class WebSecurityConfig {
                 .roles("ADMIN")
                 .build());
         return manager;
-    }
+    }*/
 
     @Bean
     AuthenticationManager authManager (HttpSecurity http) throws Exception {
         return http.getSharedObject(AuthenticationManagerBuilder.class)
-                .userDetailsService(userDetailsService())
+                .userDetailsService(userDetailsService)
                 .passwordEncoder(passwordEncoder())
                 .and()
                 .build();
