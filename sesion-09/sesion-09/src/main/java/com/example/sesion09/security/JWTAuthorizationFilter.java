18045a84888c73f14c86347dd0ab301879fcae82
jaeem006 <javierem_94@ciencias.unam.mx>
2023-01-27 22:16:15 -0600

    sesion 9

--- sesion-09/sesion-09/src/main/java/com/example/sesion09/security/JWTAuthorizationFilter.java
+++ sesion-09/sesion-09/src/main/java/com/example/sesion09/security/JWTAuthorizationFilter.java
@@ -7,10 +7,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 public class JWTAuthorizationFilter extends OncePerRequestFilter {
 
     @Override
