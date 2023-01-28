cd9fc3f88fcdc12e38202a76d1d75a3dc2e84b85
jaeem006 <javierem_94@ciencias.unam.mx>
2023-01-27 21:26:07 -0600

    sesion 9

--- sesion-09/sesion-09/src/main/java/com/example/sesion09/repository/UsuarioRepository.java
+++ sesion-09/sesion-09/src/main/java/com/example/sesion09/repository/UsuarioRepository.java
@@ -4,7 +4,11 @@ import com.example.sesion09.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository
         extends JpaRepository <Usuario, Integer> {
+
+    Optional<Usuario> findOneByEmail(String email);
 }
---
+++ sesion-09/sesion-09/src/main/java/com/example/sesion09/security/UserDetailsServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.sesion09.security;
+
+import com.example.sesion09.models.Usuario;
+import com.example.sesion09.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findOneByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("El correo no se encontró"));
+        return new UserDetailsImpl(usuario);
+    }
+}
